4c6f3051d9fa848ae6aec4251bc05aa52fdd73a8
p-hasan <p-hasan@hasan-ubuntu.uleth.ca>
2024-02-21 10:09:56 -0700

    Added Super Key for Emacs

--- keyboards/keychron/k8_pro/ansi/white/keymaps/work/keymap.c
+++ keyboards/keychron/k8_pro/ansi/white/keymaps/work/keymap.c
@@ -41,7 +41,7 @@ enum layers{
 enum kbd75_keycodes {
     MAC_BASE = SAFE_RANGE,
     WIN_BASE,
-    DVORAK
+    EMACS_SUPER
 };
 
 enum combos {
@@ -63,12 +63,12 @@ uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_MAC_BASE] = LAYOUT_ansi_87(
-                              MY_ESC,  KC_F1,    KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,KC_0,TG(NUMPAD_LAYER),  BL_STEP,
+          MY_ESC,  KC_F1,    KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_0,      KC_0,     KC_DEL,
           KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
           KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
           KC_LGUI,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
           KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,             KC_UP,
-          KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RCTL,MO(FN_LAYER),KC_NO,   KC_LEFT,  KC_DOWN,  KC_RGHT),
+        EMACS_SUPER,KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RCTL,MO(FN_LAYER),KC_LCTL, KC_LEFT,   KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_ansi_87(
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,  _______,  _______,
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [L_WIN_BASE] = LAYOUT_ansi_87(
-          MY_ESC,  KC_F1,    KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,               KC_0,   TG(LB_1),  BL_STEP,
+          MY_ESC,  KC_F1,    KC_F2,     KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_2,      TG(LB_1), KC_DEL,
           KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,    KC_HOME,  KC_PGUP,
           KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,    KC_END,   KC_PGDN,
           KC_LGUI,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
@@ -193,9 +193,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______),
 
 [FN_LAYER] = LAYOUT_ansi_87(
-     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  BL_DEC,   BL_INC,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,               KC_F,  CM_TOGG,  _______,
+     _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  BL_DEC,   BL_INC,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,               KC_F,  CM_TOGG,  BL_STEP,
      _______,  BT_HST1,  BT_HST2,  BT_HST3,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,  DM_REC1,  _______,  DM_PLY1,
-     BL_TOGG,  _______,  WIN_BASE, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DM_REC2,  DM_RSTP,  DM_PLY2,
+     BL_TOGG,  BL_STEP,  WIN_BASE, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  DM_REC2,  DM_RSTP,  DM_PLY2,
      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
      _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  MAC_BASE,   _______,  _______,  _______,            _______,            _______,
      _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______,  _______,  _______)
@@ -219,9 +219,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
         break;
-    case DVORAK:
+    case EMACS_SUPER:
         if (record->event.pressed) {
-            // set_single_persistent_default_layer(_DVORAK);
+            SEND_STRING(SS_LALT("x")"@s");
         }
         return false;
         break;
